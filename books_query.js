cec06ca38bad29974db0e2021dce0454a3194ebd
vijay solanki <vijaysolanki1481@gmail.com>
2024-01-25 09:14:19 +0530

    25-jan

---
+++ books_query.js
@@ -0,0 +1,15 @@
+// db.books.find({"pages":{$eq: 176}})
+// db.books.find({"pages":{$ne: 224}})
+
+// db.books.find({"pages":{$gt: 176}})
+// db.books.find({"pages":{$gte: 328}})
+
+// db.books.find({"pages":{$it: 328}})
+// db.books.find({"pages":{$ite: 76}})
+
+// db.books.find({"pages":{$in:[176,328,340]}})
+// db.books.find({"pages":{$nin:[176,328,340]}})
+
+
+
+  
